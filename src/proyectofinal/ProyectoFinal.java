adb680bf6591f5396b690aef9261a5da58788459
Gezer Xiloj <gcacojx@miumg.edu.gt>
2025-10-19 00:08:18 -0600

    Subida inicial del proyecto final

--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -15,6 +15,7 @@ public class ProyectoFinal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("holaaa mundoooo");
     }
     
 }
